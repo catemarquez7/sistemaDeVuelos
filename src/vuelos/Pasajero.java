eeb447a690002b40db328eb88b3bdb9084ab4fc8
Caterina <loumarquez149@gmail.com>
2025-05-27 13:41:16 -0300

    v4
    modifique todo para que el main este lo mas limpio posible, y que todas las operiaciones se realicen llamando funciones de las clases

--- src/vuelos/Pasajero.java
+++ src/vuelos/Pasajero.java
@@ -1,11 +1,12 @@
 package vuelos;
 
+import javax.swing.JOptionPane;
+
 public class Pasajero extends Persona{
 
 	//atributos
 	protected Vuelo vuelo;
 	protected int nroCliente;
-	protected String asiento;
 	
 	//constructores
 	public Pasajero() {
@@ -13,9 +14,10 @@ public class Pasajero extends Persona{
 	}
 	
 	public Pasajero(String nombre, String apellido, int dni, int edad,
-			int nroCliente) {
+			int nroCliente, Vuelo vuelo) {
 		super(nombre, apellido, dni, edad);
 		this.nroCliente = nroCliente;
+		this.vuelo = vuelo;
 	}
 
 	//getters y setters
@@ -27,32 +29,94 @@ public class Pasajero extends Persona{
 		this.vuelo = vuelo;
 	}
 
-	public int getNroCliente() {
+	public double getNroCliente() {
 		return nroCliente;
 	}
 
-	public void setNroCliente(int nroCliente) {
-		this.nroCliente = nroCliente;
+	public void setNroCliente(int d) {
+		this.nroCliente = d;
 	}
-
-	public String getAsiento() {
-		return asiento;
-	}
-
-	public void setAsiento(String asiento) {
-		this.asiento = asiento;
-	}
-
+	
 	@Override
 	public String toString() {
-		return "Pasajero [vuelo=" + vuelo + ", nroCliente=" + nroCliente + ", asiento=" + asiento + ", nombre=" + nombre
-				+ ", apellido=" + apellido + ", dni=" + dni + ", edad=" + edad + "]";
+		return "Pasajero [nroCliente=" + nroCliente + " nombre=" + nombre
+				+ " apellido=" + apellido + "\n dni=" + dni + " edad=" + edad + "]";
 	}
 	
 	//metodos
-	public void reservarVuelo() {
+	public static Pasajero registrarPasajero(Vuelo vuelo) {
+		String nombre = JOptionPane.showInputDialog("Ingrese su nombre:");
+		nombre = Validaciones.validarText(nombre);
+		String apellido = JOptionPane.showInputDialog("Ingrese su apellido:");
+		apellido = Validaciones.validarText(apellido);
+		JOptionPane.showMessageDialog(null, "Ingrese su edad:");
+		int edad = Validaciones.validarNum();
+		JOptionPane.showMessageDialog(null, "Ingrese su DNI:");
+		int dni = Validaciones.validarNum();
+		Pasajero p1 = new Pasajero(nombre, apellido, dni, edad, (int)(Math.random()*100+1), vuelo);
+		return p1;
+	}
+	
+	public static void reservarVuelo() {
+		//menus y variables
+		String[] menu1 = {"Buenos Aires", "Roma", "Londres"};
+		String[] menu2 = {"SI", "NO"};
+		String opcionOrigen, opcionDestino;
+		int opcionSiNo;
+		
+		//inicio
+		do {
+		opcionOrigen = (String)JOptionPane.showInputDialog(null, "Seleccione su ciudad de origen:", "SELECCIONE", 0, null, menu1, menu1[0]);
+		opcionDestino = (String)JOptionPane.showInputDialog(null, "Seleccione su ciudad de destino:", "SELECCIONE", 0, null, menu1, menu1[0]);
+		} while (opcionOrigen == opcionDestino);
+		for (int i = 0; i < Vuelo.vuelos.size(); i++) {
+			if (Vuelo.vuelos.get(i).getOrigen().equals(opcionOrigen) && Vuelo.vuelos.get(i).getDestino().equals(opcionDestino)) {
+				JOptionPane.showMessageDialog(null, "Excelente! Se ha encontrado un vuelo con origen en: " + opcionOrigen + " y destino en: " + opcionDestino + "\n\n " + Vuelo.vuelos.get(i).toString());
+				opcionSiNo = JOptionPane.showOptionDialog(null, "Desea reservar un asiento para este vuelo?", "SELECCIONE", 0, 0, null, menu2, menu2[0]);
+				if (opcionSiNo==0) {
+					if (Vuelo.vuelos.get(i).getAsientosDisponibles() > 0) {
+						JOptionPane.showMessageDialog(null, "Para poder finalizar la reserva, debera registrarse.");
+						Vuelo.vuelos.get(i).pasajeros.add(registrarPasajero(Vuelo.vuelos.get(i)));
+						Vuelo.vuelos.get(i).setAsientosDisponibles((Vuelo.vuelos.get(i).getAsientosDisponibles() - 1));
+						JOptionPane.showMessageDialog(null, "Excelente! Su reserva quedo confirmada.");
+					} else {
+						JOptionPane.showMessageDialog(null, "Lo sentimos, no contamos con mas asientos disponibles \npara este vuelo.");
+					}
+				}
+				
+			}
+		}
+		
+		
+		
+		
 		
 	}
 	
+	public static void mostrarReserva() {
+		//variables
+		int check;
+		String input;
+		boolean flag=false;
+		
+		//inicio
+		do {
+		input = JOptionPane.showInputDialog("Ingrese su DNI:");
+		} while (input.isEmpty());
+		check = Integer.parseInt(input);
 	
+			for (int i = 0; i < Vuelo.vuelos.size(); i++) {
+				for (int j = 0; j < Vuelo.vuelos.get(i).pasajeros.size(); j++) {
+					if (Vuelo.vuelos.get(i).getPasajeros().get(j).getDni() == check) {
+						JOptionPane.showMessageDialog(null, "Tiene una reserva: \n" + Vuelo.vuelos.get(i).toString() + "A nombre de: " + Vuelo.vuelos.get(i).getPasajeros().get(j).toString());
+						flag = true;
+						break;
+					}
+				}
+			}
+			if (!flag) {
+				JOptionPane.showMessageDialog(null, "Lo sentimos, no encontramos una reserva a su nombre.");
+			}
+	}
+
 }
--- src/vuelos/Vuelo.java
+++ src/vuelos/Vuelo.java
@@ -3,7 +3,12 @@ package vuelos;
 import java.time.LocalDateTime;
 import java.util.LinkedList;
 
+import javax.swing.JOptionPane;
+
 public class Vuelo {
+	
+	//clase
+	static LinkedList<Vuelo> vuelos = new LinkedList<Vuelo>();
 
 	//atributos
 	protected Avion avion;
@@ -13,6 +18,7 @@ public class Vuelo {
 	protected LocalDateTime llegada;
 	protected LinkedList<Pasajero> pasajeros;
 	protected int asientosDisponibles;
+	
 	
 	//constructor
 	public Vuelo(Avion avion, String origen, String destino, LocalDateTime partida, LocalDateTime llegada) {
@@ -85,10 +91,52 @@ public class Vuelo {
 		this.asientosDisponibles = asientosDisponibles;
 	}
 
+	public static LinkedList<Vuelo> getVuelos() {
+		return vuelos;
+	}
+	
+	public static void setVuelos(LinkedList<Vuelo> vuelos) {
+		Vuelo.vuelos = vuelos;
+	}
+	
 	@Override
 	public String toString() {
-		return "Vuelo: origen=" + origen + ", destino=" + destino + ",\n partida=" + partida
-				+ ", llegada=" + llegada;
+		return "Vuelo: origen=" + origen + " destino=" + destino + " partida=" + partida
+				+ " llegada=" + llegada + "\n \n";
+	}
+
+	public static void cargarVuelos() {
+		Avion a1 = new Avion("Airbus A320", "1234", 5);
+		Avion a2 = new Avion("Boeing 767", "6758", 6);
+		Avion a3 = new Avion("Embraer 190", "9087", 4);
+		Vuelo v1 = new Vuelo(a1, "Buenos Aires", "Londres", null, null);
+		Vuelo v2 = new Vuelo(a2, "Buenos Aires", "Roma", null, null);
+		Vuelo v3 = new Vuelo(a3, "Londres", "Buenos Aires", null, null);
+		Vuelo v4 = new Vuelo(a2, "Roma", "Buenos Aires", null, null);
+		Vuelo v5 = new Vuelo(a1, "Londres", "Roma", null, null);
+		Vuelo v6 = new Vuelo(a3, "Roma", "Londres", null, null);
+		v1.setPartida(LocalDateTime.of(2025, 6, 15, 20, 35));
+		v1.setLlegada(LocalDateTime.of(2025, 6, 16, 9, 50));
+		v2.setPartida(LocalDateTime.of(2025, 9, 4, 8, 20));
+		v2.setLlegada(LocalDateTime.of(2025, 9, 4, 19, 30));
+		v3.setPartida(LocalDateTime.of(2025, 11, 10, 22, 40));
+		v3.setLlegada(LocalDateTime.of(2025, 11, 11, 11, 11));	
+		v4.setPartida(LocalDateTime.of(2025, 3, 1, 20, 40));
+		v4.setLlegada(LocalDateTime.of(2025, 3, 2, 21, 11));
+		v5.setPartida(LocalDateTime.of(2025, 5, 10, 9, 20));
+		v5.setLlegada(LocalDateTime.of(2025, 5, 11, 19, 10));
+		v6.setPartida(LocalDateTime.of(2025, 7, 14, 12, 05));
+		v6.setLlegada(LocalDateTime.of(2025, 7, 14, 23, 00));
+		vuelos.add(v1);
+		vuelos.add(v2);
+		vuelos.add(v3);
+		vuelos.add(v4);
+		vuelos.add(v5);
+		vuelos.add(v6);
+	}
+	
+	public static void mostrarVuelos() {
+		JOptionPane.showMessageDialog(null, vuelos);
 	}
 
 	
